e788d68ca185bbfd2ed0368cda5de2b40c0b687c
Ines Salazar <inessalazarzam@gmail.com>
2019-09-27 16:46:52 -0400

    FIN

---
+++ webEval/webEval/Models/Eval.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace webEval.Models
+{
+    public enum SexType
+    {
+        masculino,
+        femenino
+    }
+    public class Eval
+    {
+            [Key]
+            public int StudentId { get; set; }
+            [Required]
+            public string Name { get; set; }
+            [Required]
+            [DataType(DataType.Date)]
+            [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+            public DateTime BirthDate { get; set; }
+            [Required]
+            public SexType Type { get; set; }
+            
+        }
+
+}
---
+++ webEval/webEval/Models/StudentDetail.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webEval.Models
+{
+    public enum StateType
+    {
+        Aprobado,
+        Reprobado
+    }
+    public class StudentDetail
+    {
+        [Key]
+        public int DetailId { get; set; }
+        public int StudentId { get; set; }
+        [Required]
+        public string Course { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public decimal Grade { get; set; }
+        public StateType Type { get; set; }
+    }
+}
